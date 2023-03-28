01e65b13bc896332ac4bec8b7b51de8607869fa6
Fabiano Batista <fabianobatista@outlook.com>
2023-03-27 23:33:02 -0300

    criado as interfaces Observable e Observer que seguem o padrão Publish/Subscriber

---
+++ src/me/dio/observer/Observable.java
@@ -0,0 +1,11 @@
+package me.dio.observer;
+
+public interface Observable {
+
+  void register(Observer observer);
+
+  void unregister(Observer observer);
+
+  void notifyAllObservers();
+  
+}
---
+++ src/me/dio/observer/Observer.java
@@ -0,0 +1,5 @@
+package me.dio.observer;
+
+public interface Observer {
+  void update(int temperatura);
+}
